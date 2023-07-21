7ffa2cbc00ca827028b980d0f057dc8a54647665
ArchBots <140147861+ArchBots@users.noreply.github.com>
2023-07-21 13:37:16 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,8 +2,8 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = "26792767"
+    API_HASH = "d5892fe7d458bff884fde1f1efbaadf3"
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
